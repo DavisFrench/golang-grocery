fa55c885ce7b5731f80d53e03b682ac73b13d4d4
DavisFrench <davisdavis27@gmail.com>
2021-04-13 23:23:35 -0400

    Beginnings of the server code.

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"os/signal"
 
 	gg "DavisFrench/golang-grocery"
 	"DavisFrench/golang-grocery/db"
+	"DavisFrench/golang-grocery/http"
 )
 
 type App struct {
@@ -18,11 +22,27 @@ func main() {
 		groceryService: groceryService,
 	}
 
-	app.run()
+	if err := app.run(); err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+	fmt.Println("Shutting down from interrupt")
 }
 
 func (a *App) run() error {
-	fmt.Println("main")
+
+	httpServer := http.NewServer(a.groceryService)
+	httpServer.Addr = ":8888"
+
+	if err := httpServer.Open(); err != nil {
+		return err
+	}
+
+	fmt.Println("Listening on " + httpServer.Addr)
 
 	return nil
 }
--- http/server.go
+++ http/server.go
@@ -1,1 +1,64 @@
 package http
+
+import (
+	"net"
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+
+	gg "DavisFrench/golang-grocery"
+)
+
+type Server struct {
+	ln net.Listener
+
+	Addr        string
+	Host        string
+	Recoverable bool
+
+	groceryService gg.GroceryService
+}
+
+func NewServer(groceryService gg.GroceryService) *Server {
+	return &Server{
+		Recoverable:    true,
+		groceryService: groceryService,
+	}
+}
+
+func (s *Server) Open() error {
+	ln, err := net.Listen("tcp", s.Addr)
+	if err != nil {
+		return err
+	}
+	s.ln = ln
+
+	return http.Serve(s.ln, s.router())
+}
+
+func (s *Server) Close() error {
+	if s.ln != nil {
+		s.ln.Close()
+	}
+	return nil
+}
+
+func (s *Server) router() http.Handler {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	if s.Recoverable {
+		r.Use(middleware.Recoverer)
+	}
+
+	r.Route("/grocery", func(r chi.Router) {
+		r.Get("/ping", s.handlePing)
+	})
+
+	return r
+}
+
+func (s *Server) handlePing(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(`{"status":"ok"}` + "\n"))
+}
